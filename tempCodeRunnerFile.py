1c2de3512226de43eb9bb093e6cb648103433df2
Alvaro Alves Pires Filho <alvarofilho581@gmail.com>
2026-04-24 03:35:14 -0300

    removi um arquivo inútil

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-      dados_cidade = obter_cidade() # armazena os dados da cidade dentro de 'dados_cidade', chamando a função 'obter_cidade'
-      estado = obter_estado(dados_cidade) #pega o estado de acordo com os dados da cidade
-      exibir_dados(dados_cidade,estado) #exibe os dados, de acordo com a cidade e o estado
